f6f14b78faa9d5f8b52db3e1412a148723f33b7e
doosicEE <strou73@naver.com>
2024-04-11 15:02:18 +0900

    add userInfoStore

---
+++ src/store/userInfoStore.js
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const userInfoStore = defineStore("userInfo", () => {
+
+  const isLogin = ref(false);
+  const userInfo = ref({
+    username: '',
+  })
+
+  const getIsLogin = () => {
+    return isLogin.value;
+  }
+  const setIsLogin = (param) => {
+    isLogin.value = param;
+  }
+
+  const getUserInfo = () => {
+    return userInfo.value;
+  }
+  const setUserInfo = (param) => {
+    userInfo.value = param;
+  }
+
+  return {
+    getIsLogin,
+    setIsLogin,
+    getUserInfo,
+    setUserInfo,
+  }
+})
