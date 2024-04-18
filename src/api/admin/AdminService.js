fe10fd2372df34e8511f23b1d1eae041eb20521f
doosicEE <strou73@naver.com>
2024-04-18 11:43:53 +0900

    update AdminService에 api 추가(업데이트, 조회,삭제,잠금해제)

--- src/api/admin/AdminService.js
+++ src/api/admin/AdminService.js
@@ -14,7 +14,10 @@ export class AdminService {
         .then((d) => d);
   }
 
-  getAdmins() {
+  getAdmin(params) {
+    const urlParams = new URLSearchParams(params);
+    const url = `http://localhost:30871/backoffice/bs/admin?${urlParams}`
+
     const requestOptions = {
       credentials: 'include',
       method: 'GET',
@@ -23,7 +26,99 @@ export class AdminService {
         'Cache-Control': 'no-cache',
       },
     };
-    return fetch('http://localhost:30871/backoffice/bs/admins', requestOptions)
+
+    return fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((d) => d);
+  }
+
+
+  getAdmins(params) {
+    const urlParams = new URLSearchParams(params);
+    const url = `http://localhost:30871/backoffice/bs/admins?${urlParams}`
+
+    const requestOptions = {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+    };
+
+    return fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((d) => d);
+  }
+
+  createAdmin(dataToSend) {
+    const url = `http://localhost:30871/backoffice/bp/admin`
+
+    const requestOptions = {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify(dataToSend)
+    };
+
+    return fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((d) => d);
+  }
+
+  updateAdmin(dataToSend) {
+    const url = `http://localhost:30871/backoffice/bs/admin`
+
+    const requestOptions = {
+      credentials: 'include',
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify(dataToSend)
+    };
+
+    return fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((d) => d);
+  }
+
+  deleteAdmin(dataToSend) {
+    const url = `http://localhost:30871/backoffice/bs/admin`
+
+    const requestOptions = {
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify(dataToSend)
+    };
+
+    return fetch(url, requestOptions)
+        .then((res) => res.json())
+        .then((d) => d);
+  }
+
+  unlockAdmin(dataToSend) {
+    const url = `http://localhost:30871/backoffice/bs/admin/unlock`
+
+    const requestOptions = {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify(dataToSend)
+    };
+
+    return fetch(url, requestOptions)
         .then((res) => res.json())
         .then((d) => d);
   }
